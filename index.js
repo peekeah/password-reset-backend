42799181b2b5e290af6934063954e3dd37a79c45
Pranay Kothari <pranay.kothari109@gmail.com>
2022-08-07 10:33:04 +0530

    cors added

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const router = require("./routes/user");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
 app.use("/", router);
 
 app.get("/", async (req, res, next) => {
-  res.send('Password Reset')
-})
+  res.send("Password Reset");
+});
 
 try {
   mongoose.connect(process.env.MONGO_URL);
